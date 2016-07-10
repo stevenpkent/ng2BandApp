0087c1137aad417e19b23a19ea8d591892f856fe
steven kent <steven@zipflip.com>
2016-07-10 00:25:36 -0400

    small routing changes band-list

--- src/app/band/band-list.component.ts
+++ src/app/band/band-list.component.ts
@@ -60,9 +60,4 @@ export class BandListComponent implements OnInit {
         });
         */
     }
-
-    onSelect(band: band): void {
-        this.router.navigate(['/band', band.id]);
-    }
-
 }
